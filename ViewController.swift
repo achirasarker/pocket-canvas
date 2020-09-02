1d930715854ec82af0ca62011dfdd2ff192ca7d3
achirasarker <67404429+achirasarker@users.noreply.github.com>
2020-09-02 02:38:22 -0400

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -75,7 +75,7 @@ class ViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        //get rid of any resources that can be recreated 
+        // get rid of any resources that can be recreated 
     }
 
 }
